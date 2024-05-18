b12642a794a670025478c618df3996c56e3a0fcd
sbkrish <ranimaniyan@gmail.com>
2024-05-18 10:12:59 +0530

    firt commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Test from './assets/Test'
+
+
+function App() {
+  
+  const [count, setCount] = useState(0)
+  function counter() {
+    setCount((count)=>count+1)
+  }
+  
+  return (
+    <>
+      <div className="card">
+        <button onClick={counter}>
+         {count}
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
